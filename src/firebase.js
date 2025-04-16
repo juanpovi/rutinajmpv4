2392401cc7e16a719a40d652a340c82b5c9f7d63
juanpovi <129616302+juanpovi@users.noreply.github.com>
2025-04-16 15:15:10 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDBXZ3Va0RRXrsLeYcA6N1CiNY0wSU1jco",
   authDomain: "ejerciciosjmp.firebaseapp.com",
@@ -13,9 +9,12 @@ const firebaseConfig = {
   storageBucket: "ejerciciosjmp.firebasestorage.app",
   messagingSenderId: "152542549504",
   appId: "1:152542549504:web:f9b8eb878bb9b7ab172ae5",
-  measurementId: "G-3E4XSDBWYW"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app); // <-- ESTA LÍNEA es clave
+
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, signInWithPopup, signOut, db, doc, getDoc, setDoc };
